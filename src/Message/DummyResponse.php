fd3d4258fc17debc7629161c4ed06f762d6a9714
Marcus Nightingale <marcusnightingale.1@gmail.com>
2023-10-25 10:42:14 +0100

    * Updating urls to be the new tponlinepayments2

--- src/Message/DummyResponse.php
+++ src/Message/DummyResponse.php
@@ -20,9 +20,9 @@ class DummyResponse extends AbstractResponse
     public function isSuccessful()
     {
         if (is_array($this->form)) {
-            return !empty($this->form['endpoint']) && $this->form['endpoint'] == "https://mms.payzoneonlinepayments.com/Pages/PublicPages/PaymentForm.aspx" && !empty($this->form['HashDigest']) && preg_match('/\w+/', $this->form['HashDigest']);
+            return !empty($this->form['endpoint']) && $this->form['endpoint'] == "https://mms.tponlinepayments2.com/Pages/PublicPages/PaymentForm.aspx" && !empty($this->form['HashDigest']) && preg_match('/\w+/', $this->form['HashDigest']);
         } else {
-            return preg_match("/<form method='post' action='https:\/\/mms.payzoneonlinepayments.com\/Pages\/PublicPages\/PaymentForm.aspx' id='payzone-form'.*<\/form><script>.*payzone-form'\)\.submit.*<\/script>/", $this->form);
+            return preg_match("/<form method='post' action='https:\/\/mms.tponlinepayments2.com\/Pages\/PublicPages\/PaymentForm.aspx' id='payzone-form'.*<\/form><script>.*payzone-form'\)\.submit.*<\/script>/", $this->form);
         }
     }
 
